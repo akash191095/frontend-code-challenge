74f704718a9c557358ca26120fb2ac513f9c5a38
Akash Agarwal <akash191095@gmail.com>
2020-10-22 19:24:07 +0530

    chore(setup): add emotion for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      css={css`
+        color: red;
+      `}
+    >
+      <h1>Hello from App.js</h1>
     </div>
   );
 }

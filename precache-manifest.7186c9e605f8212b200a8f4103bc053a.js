2d2181f1eb1af092f6aa131827c7dcc816a51215
Akash Agarwal <akash191095@gmail.com>
2020-10-26 11:00:33 +0530

    Updates

---
+++ precache-manifest.7186c9e605f8212b200a8f4103bc053a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771d2fc65d1af0f345c83352b91d4d47",
+    "url": "/frontend-code-challenge/index.html"
+  },
+  {
+    "revision": "1b37b1bb6eba5cf4e68e",
+    "url": "/frontend-code-challenge/static/css/main.33922e95.chunk.css"
+  },
+  {
+    "revision": "55894a41d59f6b76c093",
+    "url": "/frontend-code-challenge/static/js/2.552e94d6.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/frontend-code-challenge/static/js/2.552e94d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b37b1bb6eba5cf4e68e",
+    "url": "/frontend-code-challenge/static/js/main.d1622bdc.chunk.js"
+  },
+  {
+    "revision": "991097194609c28dad28",
+    "url": "/frontend-code-challenge/static/js/runtime-main.cc78ad09.js"
+  },
+  {
+    "revision": "2156bd2bb447f142850853d2422d5903",
+    "url": "/frontend-code-challenge/static/media/avatar@2x.2156bd2b.png"
+  },
+  {
+    "revision": "12da72701f6e6609b5a27a0007dc83e5",
+    "url": "/frontend-code-challenge/static/media/call.12da7270.svg"
+  },
+  {
+    "revision": "6a8e477f41a37b82ae2ee4dbe4f11faa",
+    "url": "/frontend-code-challenge/static/media/chat.6a8e477f.svg"
+  },
+  {
+    "revision": "b5098bdc9dbca453e84481d78ff45085",
+    "url": "/frontend-code-challenge/static/media/hamburger_icon.b5098bdc.png"
+  },
+  {
+    "revision": "7383a7dcc60bb3ab200d0b5182be42ae",
+    "url": "/frontend-code-challenge/static/media/icon-3@2x.7383a7dc.png"
+  },
+  {
+    "revision": "6e0a0c28546e2c6b3448351bcf6436bc",
+    "url": "/frontend-code-challenge/static/media/mail.6e0a0c28.svg"
+  },
+  {
+    "revision": "21caefcb6e3b130305b847ad75192dee",
+    "url": "/frontend-code-challenge/static/media/plane.21caefcb.svg"
+  }
+]);
